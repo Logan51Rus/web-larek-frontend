6995904e05733ee26e5cd0e9c9c4be350cef0846
Dmitriy Khristich <logan51rus@gmail.com>
2024-04-13 10:28:21 +0300

    Добавлено описание архитектуры; объявлена типизация интерфейсов

---
+++ src/types/index.ts
@@ -0,0 +1,54 @@
+interface ICard {
+    id: string;
+    category: string;
+    title: string;
+    image: string;
+    price: string
+}
+
+interface ICardActions {
+    onClick: (event: MouseEvent) => void;
+}
+
+interface IOrderForm {
+    email: string;
+    phone: string
+}
+
+interface IOrder extends IOrderForm {
+    item: string[]
+}
+
+type IBusketItem = Pick<ICard, 'id' | 'title' | 'price'> & {counter: number} 
+
+interface IBasketView {
+    items: IBusketItem[];
+    total: number;
+}
+
+interface IFormState {
+    valid: boolean;
+    errors: string[];
+}
+
+interface IModalData {
+    content: HTMLElement;
+}
+
+interface ISuccess {
+    total: number;
+}
+
+interface IAppState {
+    catalog: ICard[];
+    basket: string[];
+    preview: string | null;
+    order: IOrder | null;
+    loading: Boolean
+}
+
+interface IPage {
+    counter: number;
+    catalog: HTMLElement[];
+    locked: boolean;
+}
